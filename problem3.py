d141bdd422ab97e71c39f1faff8627f081a93faa
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-17 14:16:09 +0300

    Added solution to the dictionary and dictionary scrapping problem

---
+++ problem3.py
@@ -0,0 +1,60 @@
+from string import punctuation
+import urllib.request, urllib.error, urllib.parse
+
+# Taking the user input of the webpage input and assigning it as url
+url = input("Enter the webpage url: ")
+
+# For example website url = https://www.oldbaileyonline.org/browse.jsp?id=t17800628-33&div=t17800628-33
+
+# Method for returning the text
+def stripTags(pageContents):
+    pageContents = str(pageContents)
+    startLoc = pageContents.find("<p>")
+    endLoc = pageContents.rfind("<br/>")
+
+    pageContents = pageContents[startLoc:endLoc]
+
+    inside = 0
+    text = ''
+
+    for char in pageContents:
+        if char == '<':
+            inside = 1
+        elif (inside == 1 and char == '>'):
+            inside = 0
+        elif inside == 1:
+            continue
+        else:
+            text += char
+
+    return text
+
+# Removing the punctuation marks
+def stripNonAlphaNum(text):
+    import re
+    return re.compile(r'\W+', re.UNICODE).split(text)
+
+# Method for converting the dict to list
+def wordListToFreqDict(wordlist):
+    wordfreq = [wordlist.count(p) for p in wordlist]
+    return dict(list(zip(wordlist,wordfreq)))
+
+# The sorting method of the list
+def sortFreqDict(freqdict):
+    aux = [(freqdict[key], key) for key in freqdict]
+    aux.sort()
+    aux.reverse()
+    return aux
+
+
+response = urllib.request.urlopen(url)
+html = response.read().decode('UTF-8')
+text = stripTags(html).lower()
+wordlist = stripNonAlphaNum(text)
+dictionary = wordListToFreqDict(wordlist)
+sorteddict = sortFreqDict(dictionary)
+
+# Outputting the list frequencies
+for s in sorteddict: print(str(s))
+
+#On the terminal run python3 problem3.py to run and compile
